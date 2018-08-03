34404713756b6476f604b1d6b4496e075d8c61cb
sorgelig <pour.garbage@gmail.com>
2018-08-03 22:22:55 +0800

    Update Kempston mouse.

--- kempston_mouse.v
+++ kempston_mouse.v
@@ -34,41 +34,32 @@ module kempston_mouse
 assign dout = data;
 assign sel  = port_sel;
 
-reg   [1:0] button;
-reg         mbutton;
-reg  [11:0] dx;
-reg  [11:0] dy;
+reg [11:0] dx;
+reg [11:0] dy;
 
-reg   [1:0] swap;
-reg   [7:0] data;
-reg         port_sel;
+reg  [7:0] data;
+reg        port_sel;
 always @* begin
 	port_sel = 1;
 	casex(addr)
 		 3'b011: data = dx[7:0];
 		 3'b111: data = dy[7:0];
-		 3'bX10: data = ~{5'b00000,mbutton, button[swap[1]], button[~swap[1]]} ;
+		 3'bX10: data = ~{5'b00000,ps2_mouse[2], ps2_mouse[0], ps2_mouse[1]} ;
 		default: {port_sel,data} = 8'hFF;
 	endcase
 end
 
 always @(posedge clk_sys) begin
-	reg       old_status;
-	
+	reg old_status;
 	old_status <= ps2_mouse[24];
 
 	if(reset) begin
-		dx     <= 128; // dx != dy for better mouse detection
-		dy     <= 0;
-		button <= 0;
-		swap   <= 0;
-	end else begin
-		if(old_status != ps2_mouse[24]) begin
-			if(!swap) swap <= ps2_mouse[1:0];
-			{mbutton,button} <= ps2_mouse[2:0];
-			dx <= dx + {{4{ps2_mouse[4]}},ps2_mouse[15:8]};
-			dy <= dy + {{4{ps2_mouse[5]}},ps2_mouse[23:16]};
-		end
+		dx <= 128; // dx != dy for better mouse detection
+		dy <= 0;
+	end
+	else if(old_status != ps2_mouse[24]) begin
+		dx <= dx + {{4{ps2_mouse[4]}},ps2_mouse[15:8]};
+		dy <= dy + {{4{ps2_mouse[5]}},ps2_mouse[23:16]};
 	end
 end
 
