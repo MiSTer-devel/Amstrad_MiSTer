cf4e8ecc46b1046dfef75e737c90fd59de431652
sorgelig <pour.garbage@gmail.com>
2019-03-08 23:09:20 +0800

    Update sys.

--- Amstrad.sv
+++ Amstrad.sv
@@ -1,8 +1,6 @@
 //============================================================================
 //  Amstrad CPC 6128
-// 
-//  Port to MiST/MiSTer.
-//  Copyright (C) 2018 Sorgelig
+//  Copyright (C) 2018-2019 Sorgelig
 //
 //  This program is free software; you can redistribute it and/or modify it
 //  under the terms of the GNU General Public License as published by the Free
@@ -105,8 +103,18 @@ module emu
 	output        UART_DTR,
 	input         UART_DSR,
 
+	// Open-drain User port.
+	// 0 - D+/RX
+	// 1 - D-/TX
+	// 2..5 - USR1..USR4
+	// Set USER_OUT to 1 to read from USER_IN.
+	input   [5:0] USER_IN,
+	output  [5:0] USER_OUT,
+
 	input         OSD_STATUS
 );
+
+assign USER_OUT = '1;
 assign {UART_RTS, UART_TXD, UART_DTR} = 0;
 assign {SD_SCK, SD_MOSI, SD_CS} = 'Z;
 
