ffa2791faeda074ed4af55d997adcd7f3a4af198
Sorgelig <pour.garbage@gmail.com>
2025-06-23 16:09:27 +0800

    Update sys.

--- Amstrad.sv
+++ Amstrad.sv
@@ -55,6 +55,8 @@ module emu
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
+	output        HDMI_BLACKOUT,
+	output        HDMI_BOB_DEINT,
 
 `ifdef MISTER_FB
 	// Use framebuffer in DDRAM
@@ -185,6 +187,8 @@ assign BUTTONS   = 0;
 assign VGA_SCALER= 0;
 assign VGA_DISABLE = 0;
 assign HDMI_FREEZE = 0;
+assign HDMI_BLACKOUT = 0;
+assign HDMI_BOB_DEINT = 0;
 
 // Status Bit Map:
 // 0         1         2         3          4         5         6
